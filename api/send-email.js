3cedb9820d63599fd6e00fec72b924f6870f0853
putradevl <saputraluhur27@gmail.com>
2024-06-29 20:43:39 +0700

    first commit

---
+++ api/send-email.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL, // Ganti dengan email Anda
+        pass: process.env.PASSWORD, // Ganti dengan password email Anda
+      },
+    });
+
+    let mailOptions = {
+      from: process.env.EMAIL,
+      to: 'saputraluhurp@gmail.com',
+      subject: 'Form Submission',
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email sent successfully!' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+};
